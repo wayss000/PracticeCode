3cc6ecfda027ecda11cd20b04260aa14634a0f57
wayss000 <wayss000@gmail.com>
2017-05-17 10:26:24 +0800

    modify

--- temp/Value.java
+++ temp/Value.java
@@ -1,4 +1,6 @@
-
+/**
+ * eclipse 可以通过sourse自动生成hashCode和equals方法，比自己实现的要好。
+ */
 public class Value {
     private int i;
     private String s;
@@ -11,7 +13,35 @@ public class Value {
         this.s = s;
     }
     
-    public boolean equals(Object o){
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + i;
+        result = prime * result + ((s == null) ? 0 : s.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Value other = (Value) obj;
+        if (i != other.i)
+            return false;
+        if (s == null) {
+            if (other.s != null)
+                return false;
+        } else if (!s.equals(other.s))
+            return false;
+        return true;
+    }
+    
+    //我的实现,当s为null时，有bug
+/*    public boolean equals(Object o){
         if(o == null){
             return false;
         }
@@ -23,5 +53,5 @@ public class Value {
             return false;
         }
         return false;
-    }
+    }*/
 }
