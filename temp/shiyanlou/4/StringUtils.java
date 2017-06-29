38cbfc5f3f1caf3b6ad648cecc3b0c0412fd5da6
wayss000 <wayss000@gmail.com>
2017-06-29 14:14:52 +0800

    实验楼StringUtils练习

---
+++ temp/shiyanlou/4/StringUtils.java
@@ -0,0 +1,40 @@
+
+/**
+ * 一个判断字符串为空的工具类
+ * @author Wayss
+ * Create time 2017-06-29
+ *
+ */
+public class StringUtils {
+    /**
+     * 接收N(1<=N)个String类型的参数，返回是否都不为null
+     * @param s
+     * @return
+     */
+    public static boolean allIsNotNull(String... s){
+        for(String str : s){
+            if(str == null){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 接收N(1<=N)个String类型的参数，返回是否都不为null或者都不为空串""
+     * @param s
+     * @return
+     */
+    public static boolean allIsNotEmpty(String... s){
+        for(String str : s){
+            if(str == null){
+                return false;
+            }else{
+                if(str.equals("")){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ temp/shiyanlou/4/readme.md
@@ -0,0 +1,5 @@
+### 说明
+
+实验楼Sting练习题
+考察对Java可变参数的理解和应用
+https://www.shiyanlou.com/contests/lou10/console
