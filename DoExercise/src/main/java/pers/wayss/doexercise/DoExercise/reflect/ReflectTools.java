198ac337fc9d2c6ecf9b15c66621490b36a705b0
wayss000 <wayss000@gmail.com>
2017-07-26 23:22:58 +0800

    add reflect practice

---
+++ DoExercise/src/main/java/pers/wayss/doexercise/DoExercise/reflect/ReflectTools.java
@@ -0,0 +1,16 @@
+package pers.wayss.doexercise.DoExercise.reflect;
+
+import java.util.Map;
+
+public class ReflectTools {
+    
+    /**
+     * 功能为通过反射，设置对象的字段，参数map中的key代表字段名称，value代表字段值。
+     * 第一个参数所对应的对象的每个字段都应该提供get/set方法
+     * @param obj
+     * @param map
+     */
+    public static void reflectSetValue(Object obj, Map map){
+        
+    }
+}
