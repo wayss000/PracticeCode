b1be64bd594bc44423253982e0ca1738b012dc21
wayss000 <wayss000@gmail.com>
2017-08-13 23:18:28 +0800

    整理代码，删除无用的注释

--- crawler/src/main/java/pers/wayss/crawler/Crawler.java
+++ crawler/src/main/java/pers/wayss/crawler/Crawler.java
@@ -5,10 +5,8 @@ import java.util.Set;
 public class Crawler {
     
     //抓取网站的URL前缀
-//    private static final String URL_PATH = "http://www.douban.com/group/haixiuzu/discussion?start=0";
-//    private static final String URL_PRE = "douban.com/group/topic";
-    
     private static final String URL_PATH = "https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&fm=index&pos=history&word=%E6%97%A5%E6%9C%AC%20%E7%BE%8E%E5%A5%B3";
+//    private static final String URL_PRE = "baidu.com";
 
     /**
      * 抓取过程
@@ -20,13 +18,10 @@ public class Crawler {
         
         Filter filter = new Filter() {
             public boolean accept(String url) {
-                return true;
-                /*
                 //这里过滤规则随需要爬的网站的规则进行改变，推荐使用正则实现，本人是爬豆瓣网站
-                if(url.indexOf(URL_PRE) != -1)
-                    return true;
-                else
-                    return false;*/
+//                if(url.indexOf(URL_PRE) != -1)
+                //我这里不做过滤
+                return true;
             }
         };
         // 初始化 URL 队列
