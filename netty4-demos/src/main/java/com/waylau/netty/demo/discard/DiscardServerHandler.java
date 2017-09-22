ef028cce3ac9b8b88a7e192a9f705a2e69e92a2b
wayss000 <wayss000@gmail.com>
2017-09-23 00:51:57 +0800

    添加注释

--- netty4-demos/src/main/java/com/waylau/netty/demo/discard/DiscardServerHandler.java
+++ netty4-demos/src/main/java/com/waylau/netty/demo/discard/DiscardServerHandler.java
@@ -8,37 +8,56 @@ import io.netty.util.ReferenceCountUtil;
 /**
  * 处理服务端 channel.
  */
-public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)DiscardServerHandler
+                                                                         // 继承自
+                                                                         // ChannelInboundHandlerAdapter，这个类实现了
+                                                                         // ChannelInboundHandler接口，ChannelInboundHandler
+                                                                         // 提供了许多事件处理的接口方法，然后你可以覆盖这些方法。现在仅仅只需要继承
+                                                                         // ChannelInboundHandlerAdapter
+                                                                         // 类而不是你自己去实现接口方法。
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-    	/*
-        // 默默地丢弃收到的数据
-        ((ByteBuf) msg).release(); // (3)
-        */
-    	
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)这里我们覆盖了
+                                                                     // channelRead()
+                                                                     // 事件处理方法。每当从客户端收到新的数据时，这个方法会在收到消息时被调用，这个例子中，收到的消息的类型是
+                                                                     // ByteBuf
         /*
-        try {
-	        // Do something with msg
-	    } finally {
-	        ReferenceCountUtil.release(msg);
-	    }
-        */
-        
-	    ByteBuf in = (ByteBuf) msg;
-	    try {
-	        while (in.isReadable()) { // (1)
-	            System.out.print((char) in.readByte());
-	            System.out.flush();
-	        }
-	    } finally {
-	        ReferenceCountUtil.release(msg); // (2)
-	    }
-        
+         * // 默默地丢弃收到的数据 ((ByteBuf) msg).release(); // (3)为了实现 DISCARD
+         * 协议，处理器不得不忽略所有接受到的消息。ByteBuf 是一个引用计数对象，这个对象必须显示地调用 release()
+         * 方法来释放。请记住处理器的职责是释放所有传递到处理器的引用计数对象。通常，channelRead() 方法的实现就像下面的这段代码：
+         */
+
+        /*
+         * 丢弃服务器 try { // Do something with msg } finally {
+         * ReferenceCountUtil.release(msg); }
+         */
+
+        /*
+         * 查看收到的数据 ByteBuf in = (ByteBuf) msg; try { while (in.isReadable()) {
+         * // (1) System.out.print((char) in.readByte()); System.out.flush(); }
+         * } finally { ReferenceCountUtil.release(msg); // (2) }
+         */
+
+        // 应答服务器
+        ctx.write(msg); // (1)
+        ctx.flush();
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)exceptionCaught()
+                                                                              // 事件处理方法是当出现
+                                                                              // Throwable
+                                                                              // 对象才会被调用，即当
+                                                                              // Netty
+                                                                              // 由于
+                                                                              // IO
+                                                                              // 错误或者处理器在处理事件时抛出的异常时。在大部分情况下，捕获的异常应该被记录下来并且把关联的
+                                                                              // channel
+                                                                              // 给关闭掉。然而这个方法的处理方式会在遇到不同异常的情况下有不同的实现，比如你可能想在关闭连接之前发送一个错误码的响应消息。
         // 当出现异常就关闭连接
         cause.printStackTrace();
         ctx.close();
